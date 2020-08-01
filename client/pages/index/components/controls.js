e4f78d556d9a6759065e7bf7f020e11a3a91586b
Crystal Jelly <AequoreaVictoria@users.noreply.github.com>
2020-08-01 15:03:42 -0500

    i like unmuting

--- client/pages/index/components/controls.js
+++ client/pages/index/components/controls.js
@@ -1,32 +1,50 @@
 import h from "stage0";
 
 const View = h`
-<section class="hidden flex control-panel" #panel>
-    <div class="audio-panel">
-        <button class="unmute-button" #mute>
-            <svg class="svg"><use xlink:href="#svg-speaker"></use></svg>
+<div>
+    <section class="modal" #modal>
+        <div class="container unmute-modal">
+            <h1>click anywhere to unmute this stream</h1>
+        </div>
+    </section>
+    <section class="hidden flex control-panel" #panel>
+        <div class="audio-panel">
+            <button class="unmute-button" #mute>
+                <svg class="svg"><use xlink:href="#svg-speaker"></use></svg>
+            </button>
+            <input class="slider" type="range"
+                step="1" value="33" min="0" max="100"
+                #volume>
+        </div>
+        <button class="fullscreen-button" #fullscreen>
+            <svg class="svg"><use xlink:href="#svg-fullscreen"></use></svg>
         </button>
-        <input class="slider" type="range"
-            step="1" value="33" min="0" max="100"
-            #volume>
-    </div>
-    <button class="fullscreen-button" #fullscreen>
-        <svg class="svg"><use xlink:href="#svg-fullscreen"></use></svg>
-    </button>
-    <button class="hidden fullscreen-button" #unfullscreen>
-        <svg class="svg"><use xlink:href="#svg-unfullscreen"></use></svg>
-    </button>
-</section>
+        <button class="hidden fullscreen-button" #unfullscreen>
+            <svg class="svg"><use xlink:href="#svg-unfullscreen"></use></svg>
+        </button>
+    </section>
+</div>
 `;
 
 const video = document.querySelector("#video");
 const screen = document.querySelector("#screen");
-const {panel, volume, mute, fullscreen, unfullscreen} = View.collect(View);
+const {modal, panel, volume, mute, fullscreen, unfullscreen} = View.collect(View);
+
+let inModal = true;
+modal.onclick = () => {
+    inModal = false;
+    modal.classList = "hidden";
+    mute.classList = "mute-button";
+    volume.value = 33
+    video.muted = false;
+    video.volume = volume.value / 100;
+};
 
 let inPanel = false;
 panel.onmouseover = () => inPanel = true;
 panel.onmouseout = () => inPanel = false;
 document.body.onmousemove = () => {
+    if (inModal) return;
     panel.classList.remove("hidden");
     setTimeout(() => {
         if (!inPanel) panel.classList.add("hidden");
